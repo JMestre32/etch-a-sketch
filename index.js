3458d52e45964d47ff8b7c64f815388b621f542e
JMestre32 <114640505+JMestre32@users.noreply.github.com>
2024-09-11 10:45:52 -0700

    Got resizing to work

--- index.js
+++ index.js
@@ -22,6 +22,37 @@ function makeInitialGrid (size){
         
 }
 
+function makeGrid(size){
+        const deleteItems = document.getElementsByClassName("gridItem");
+        for(let i = deleteItems.length - 1; i >= 0; --i){
+                deleteItems[i].remove();
+        }
+
+        for (let j = 0; j < size*size; j++){
+                let gridSize = 960/size
+                const cell = document.createElement("div");
+                cell.classList.add("gridItem");
+                cell.setAttribute("style", "display: flex; flex-wrap: wrap; margin: 0; padding: 0;")
+                cell.style.width = `${gridSize}px`
+                cell.style.height = `${gridSize}px`
+                grid.append(cell);
+                cell.addEventListener("mouseenter", () => {
+                        cell.style.backgroundColor = "black";
+                })
+        }
+
+}
+
+const resize = document.querySelector(".resize")
+
+resize.addEventListener("click", () => {
+        let newSize = 0
+        newSize = prompt("Enter a number (1-100) to resize the canvas: ")
+        while(newSize < 1 || newSize > 100 || isNaN(newSize)){
+                newSize = prompt("Invalid input select a number 1-100: ")
+        }
+        makeGrid(newSize);
+})
 
 
 makeInitialGrid(16)
